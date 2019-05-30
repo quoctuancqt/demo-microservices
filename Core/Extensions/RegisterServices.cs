d9a6005831068b40a61571fe33e02e619aa0cfb9
Tuan Cao <tuancao@success-ss.com.vn>
2019-05-30 15:48:09 +0700

    Create Logger Apdapter.

---
+++ Core/Extensions/RegisterServices.cs
@@ -0,0 +1,43 @@
+﻿namespace Core.Extensions
+{
+    using AutoMapper;
+    using Core.Interfaces;
+    using Core.Logging;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Resilience;
+    using Resilience.Factory;
+    using System;
+    using System.Reflection;
+
+    public static class RegisterServices
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped(UnitOfWorkFactory);
+
+            services.AddSingleton(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            if (configuration.GetValue<string>("UseResilientHttp") == bool.TrueString)
+            {
+                services.AddTransient<IResilientHttpClientFactory, ResilientHttpClientFactory>();
+                services.AddTransient<IHttpClient, ResilientHttpClient>(sp => sp.GetService<IResilientHttpClientFactory>().CreateResilientHttpClient());
+            }
+            else
+            {
+                services.AddTransient<IHttpClient, StandardHttpClient>();
+            }
+
+            return services;
+        }
+
+        private static UnitOfWork.IUnitOfWork UnitOfWorkFactory(IServiceProvider serviceProvider)
+        {
+            return new UnitOfWork.UnitOfWork<DbContext>(serviceProvider.GetService<IHttpContextAccessor>(), serviceProvider.GetService<DbContext>());
+        }
+    }
+}
---
+++ Core/Logging/LoggerAdapter.cs
@@ -0,0 +1,34 @@
+﻿using Core.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Core.Logging
+{
+    public class LoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> _logger;
+        public LoggerAdapter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<T>();
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);
+        }
+
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+
+        public void LogDebug(string message, params object[] args)
+        {
+            _logger.LogDebug(message, args);
+        }
+
+        public void LogError(string message, params object[] args)
+        {
+            _logger.LogError(message, args);
+        }
+    }
+}
