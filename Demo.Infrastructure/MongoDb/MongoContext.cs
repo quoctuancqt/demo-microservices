4e6609aeaa280d68551325e1cdf0634c9b400f1c
Tuan Cao <tuancao@success-ss.com.vn>
2019-06-06 18:03:44 +0700

    Add MongoFactory

---
+++ Demo.Infrastructure/MongoDb/MongoContext.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using System.Linq;
+
+namespace Demo.Infrastructure.MongoDb
+{
+    public class MongoContext : MongoClient
+    {
+        public IMongoDatabase Database { get; private set; }
+
+        public MongoContext(string connectionString, string dbName) : base(connectionString)
+        {
+            var pack = new ConventionPack
+            {
+                new CamelCaseElementNameConvention(),
+                new EnumRepresentationConvention(BsonType.String)
+            };
+
+            ConventionRegistry.Register("CamelCaseConvensions", pack, t => true);
+
+            Database = GetDatabase(dbName);
+        }
+
+        public IMongoCollection<TDocument> GetCollection<TDocument>(string name)
+            where TDocument : class
+        {
+            return Database.GetCollection<TDocument>(name);
+        }
+
+        public bool CollectionExists(string name)
+        {
+            var filter = new BsonDocument("name", name);
+
+            var collections = Database.ListCollections(new ListCollectionsOptions { Filter = filter });
+
+            return (collections.ToList()).Any();
+        }
+
+        public void CreateCollection(string name)
+        {
+            Database.CreateCollection(name);
+        }
+    }
+}
---
+++ Demo.Infrastructure/MongoDb/MongoFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Demo.Infrastructure.MongoDb
+{
+    public class MongoFactory
+    {
+        private readonly MongoContext _mongoContext;
+        public IMongoDatabase Database { get; private set; }
+
+        public MongoFactory(IConfiguration configuration)
+        {
+            _mongoContext = new MongoContext(configuration.GetSection("MongoSettings").GetValue<string>("MongoConnection"), configuration.GetSection("MongoSettings").GetValue<string>("MongoDbName"));
+
+            Database = _mongoContext.Database;
+        }
+    }
+}
